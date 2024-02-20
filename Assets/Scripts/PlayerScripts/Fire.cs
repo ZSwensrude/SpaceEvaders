825e126a278e0ffbc8ae7fcdc9d5a0b9f480ae74
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2024-02-20 13:56:29 -0700

    Add projectile shooting and change laser colour

---
+++ Assets/Scripts/PlayerScripts/Fire.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Vector3 laserOffset = new Vector3(0f, 0f, 3f);
+    [SerializeField] private int laserMax = 10;
+
+    public void FireLaser(InputAction.CallbackContext context)
+    {
+        if(_projectilePrefab.GetComponent<Laser>().LaserNum <= laserMax && context.started)
+        {
+            Instantiate(_projectilePrefab, transform.position + laserOffset, _projectilePrefab.transform.rotation);
+        }
+    }
+}
