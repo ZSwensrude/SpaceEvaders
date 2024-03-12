13623fb9aaa70c903e112df59d1cc87f8ab68992
Zachary Swensrude <swensrudez@gmail.com>
2024-03-12 16:04:31 -0600

    add engine and laser sounds

--- Assets/Scripts/PlayerScripts/Fire.cs
+++ Assets/Scripts/PlayerScripts/Fire.cs
@@ -8,11 +8,13 @@ public class Fire : MonoBehaviour
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private Vector3 laserOffset = new Vector3(0f, 0f, 3f);
     [SerializeField] private int laserMax = 10;
+    [SerializeField] private AudioSource laserSound;
 
     public void FireLaser(InputAction.CallbackContext context)
     {
         if(_projectilePrefab.GetComponent<Laser>().LaserNum <= laserMax && context.started)
         {
+            laserSound.Play();
             Instantiate(_projectilePrefab, transform.position + laserOffset, _projectilePrefab.transform.rotation);
         }
     }
